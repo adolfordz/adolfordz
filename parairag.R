a35067148580bc7284f691fd58ece79099011598
adolfordz <87946950+adolfordz@users.noreply.github.com>
2021-08-05 15:07:39 -0500

    script irag 05 agosto

--- parairag.R
+++ parairag.R
@@ -1,11 +1,12 @@
 library(tidyverse)
 
-bravo <- read.csv("bravanza1.csv", encoding = "UTF-8") %>%
-  rename(Estado = "X.U.FEFF.estado") %>%
-  mutate(fecha = as.Date(fecha,"%d/%m/%Y"))
+#bravo <- read.csv("irag.csv", encoding = "UTF-8") %>%
+ # select(-X.U.FEFF.) %>%
+  #mutate(fecha = as.Date(fecha,"%d/%m/%Y"))
   
 
-fecha<-"03-ago"
+#comienza cada día, cambiar###
+fecha<-"2021-08-04"
 
 hospgral <- read.csv("Sistema de Información de la Red IRAG.csv", 
                      encoding = "UTF-8") %>%
@@ -22,56 +23,64 @@ hosventuci <- read.csv("Sistema de Información de la Red IRAG (2).csv",
 fasr <- cbind(fecha,hospgral,hospvent,hosventuci)
 fasr = fasr [ , (c(names(bravo)))]
 
-irag <- rbind(bravo,fasr) %>%
- mutate(fecha = as.Date(fecha,"%d/%m/%Y"))
+#df principal
+bravo <- read.csv("irag.csv", encoding = "UTF-8") %>%
+  select(-X.U.FEFF.) %>%
+  mutate(fecha = as.Date(fecha,"%Y-%m-%d"))
 
-remove(fasr,irag,jul26)
+#df final
+irag <- rbind(bravo,fasr) %>%
+ mutate(fecha = as.Date(fecha,"%d-%m"))
+
+write.csv(irag,"irag.csv")
+
+
+remove(fasr,hospgral,hospvent,hosventuci)
 
 #mapa de calor para ocupación red IRAG##
 
-g_hosp <- ggplot(bravo, aes(fecha,Estado,fill=hosp_gral))
-g_vent <- ggplot(bravo, aes(fecha,Estado,fill=cama_venti))
+g_hosp <- ggplot(irag, aes(fecha,Estado,fill=hosp_gral))
+g_vent <- ggplot(irag, aes(fecha,Estado,fill=cama_venti))
 
-g_hosp + 
+hm_hosp<- g_hosp + 
   geom_tile(color="white",size=0.1)+
   scale_fill_viridis_c(name="Hospitalización general")+
   theme_minimal(base_size = 11)+
   labs(title = "Ocupación de camas de hospitalización general")+
   theme(legend.position = "bottom")
-g_vent + 
+hm_vent <- g_vent + 
   geom_tile(color="white",size=0.1)+
   scale_fill_viridis_c(name="Hospitalización general")+
   labs(title = "Ocupación de camas con ventilador") +
   theme_minimal(base_size = 11)+
   theme(legend.position = "bottom")
 
-#serie de tiempo
+#SERIE TEMPORAL PORCETUAL
+#subdf medias → por fecha
 hgral_nac <- aggregate(bravo[3],list(bravo$fecha),mean)%>%
   rename(fecha = "Group.1")
 vent_nac <- aggregate(bravo[4],list(bravo$fecha),mean)%>%
   rename(fecha = "Group.1")
-
-ggplot(hgral_nac, aes(hosp_gral))+
-  geom_density()
-  
-
-nac <- cbind(hgral_nac,vent_nac)%>%
+nac <- cbind(hgral_nac,vent_nac)%>%   #subset
   select(-3)
-nac<-pivot_longer(nac,2:3)
+nac<-pivot_longer(nac,2:3)%>% #ocupación como dicotómica
+  rename(tipo_ocupación = "name", ocupación_p = "value")
 
-ggplot(nac, aes(fecha,value,group=name))+ 
+##gráfica##  
+tsirag <- ggplot(nac, aes(fecha,ocupación_p,group=tipo_ocupación))+ 
   geom_line()+
   geom_point()
 
-plot()
+hm_vent #heatmap irag ventilador
+hm_hosp #heatmap irag generales
+tsirag #line irag
 
 h <- ggplot(hgral_nac, aes(fecha,hosp_gral,gro))
 v <- ggplot(vent_nac, aes(fecha,cama_venti))
 
 
-
 h + geom_line() + geom_point() 
 v + geom_line() + geom_point() 
 
-scale_y_discrete(trans="reverse")
-view(bravo)
+
+
